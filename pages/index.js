b4203e8709690f52eca07bccad0711e9ed00e491
Dev-Ricky <narutoleo15@yahoo.com>
2022-12-13 02:20:28 -0600

    setup url variablle to hold api

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
-
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Aledo&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
   return (
     <div>
       <Head>
