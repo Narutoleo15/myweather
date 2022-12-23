a6dc10c43e1a164985f3a630d75aa513a9823884
Dev-Ricky <narutoleo15@yahoo.com>
2022-12-23 01:13:30 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   const [weather, setWeather] = useState({})
   const [loading, setLoading] = useState(false)
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Aledo&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
   console.log(city)
 
   const fetchWeather = (e) => {
@@ -18,7 +18,6 @@ export default function Home() {
     setLoading(true)
     axios.get(url).then((response) => {
       setWeather(response.data)
-      console.log(response.data)
 
     })
     setCity("")
@@ -40,11 +39,11 @@ export default function Home() {
       <Image className='object-cover' src='https://images.unsplash.com/photo-1536152470836-b943b246224c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=738&q=80' fill alt='yoda' />
 
       <div className='relative  flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10'>
-        <form className='flex justify-between items-center min-w-[500px] m-auto p-3 bg-transparent border border-gray-400 text-white rounded-2xl'>
+        <form onSubmit={fetchWeather} className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-400 text-white rounded-2xl'>
           <div>
             <input
               onChange={(e) => setCity(e.target.value)}
-              className='bg-transparent text-white border-none hover:outline-none focus:outline-none text-xl placeholder:text' type='text' placeholder='Search city' />
+              className='bg-transparent text-white border-none focus:outline-none hover:outline-none  text-2xl placeholder:text' type='text' placeholder='Search city' />
           </div>
           <button onClick={fetchWeather}>
             <BsSearch size={20} />
